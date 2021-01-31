522e6ce81eb804fd6163b30690e1df9b2a14a84c
Takumi Abe <contact@ta93abe.com>
2021-01-31 16:25:56 +0900

    solve abc130 a -- c++

---
+++ c++/abc/130/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    int x, a;
+    cin >> x >> a;
+    if (x < a)
+        cout << 0 << endl;
+    else
+        cout << 10 << endl;
+
+    return 0;
+}
