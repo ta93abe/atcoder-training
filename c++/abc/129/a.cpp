8cf5663bde4d4afdea328164a040e853d65d502b
Takumi Abe <contact@ta93abe.com>
2021-02-01 11:04:02 +0900

    solve abc134 a -- c++

---
+++ c++/abc/129/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    int p, q, r;
+    cin >> p >> q >> r;
+
+    cout << p + q + r - max(p, max(q, r)) << endl;
+
+    return 0;
+}
