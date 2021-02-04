1ec2da169e1f77b45322cb6ce3d55a7a4a8b4306
Takumi Abe <contact@ta93abe.com>
2021-02-04 22:00:27 +0900

    solve abc190 a -- c++

---
+++ c++/abc/190/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    string ans = a > b - c ? "Takahashi" : "Aoki";
+
+    cout << ans << endl;
+
+    return 0;
+}
