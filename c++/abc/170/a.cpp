d201db6ed850f899621e555d56e6b93ad1c3c8e3
Takumi Abe <contact@ta93abe.com>
2021-02-04 19:43:44 +0900

    solve abc170 a -- c++

---
+++ c++/abc/170/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    int x1, x2, x3, x4, x5;
+    cin >> x1 >> x2 >> x3 >> x4 >> x5;
+
+    cout << 15 - x1 - x2 - x3 - x4 - x5 << endl;
+
+    return 0;
+}
