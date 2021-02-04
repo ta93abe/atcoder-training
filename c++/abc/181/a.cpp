216c491888b435bb021e622b86cc715d1504ff8b
Takumi Abe <contact@ta93abe.com>
2021-02-04 21:35:54 +0900

    solve abc181 a -- c++

---
+++ c++/abc/181/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << "White" << endl;
+    else
+        cout << "Black" << endl;
+
+    return 0;
+}
