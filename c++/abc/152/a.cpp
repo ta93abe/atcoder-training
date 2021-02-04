2430acb7eef033bd66e980ea337caa33dd5cfe45
Takumi Abe <contact@ta93abe.com>
2021-02-04 16:15:29 +0900

    solve abc152 a -- c++

---
+++ c++/abc/152/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    if (n == m)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    return 0;
+}
