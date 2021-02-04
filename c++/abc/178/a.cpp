95df1894edbac3e0691a212be09c0fadeaa0f0e4
Takumi Abe <contact@ta93abe.com>
2021-02-04 21:21:21 +0900

    solve abc178 a -- c++

---
+++ c++/abc/178/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    int ans = x ? 0 : 1;
+    cout << ans << endl;
+
+    return 0;
+}
