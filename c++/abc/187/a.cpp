2d6fca5b5c99665dc8ca85c8dceb779bf46b99c5
Takumi Abe <contact@ta93abe.com>
2021-02-04 21:51:36 +0900

    solve abc187 a -- c++

---
+++ c++/abc/187/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int f(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << max(f(a), f(b)) << endl;
+
+    return 0;
+}
