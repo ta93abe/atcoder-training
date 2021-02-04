80ca54626535254cfb9250a869d973adbe7e2034
Takumi Abe <contact@ta93abe.com>
2021-02-04 17:34:47 +0900

    solve abc163 a -- c++

---
+++ c++/abc/163/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    float r;
+    cin >> r;
+
+    cout << 2 * 3.14159265 * r << endl;
+
+    return 0;
+}
