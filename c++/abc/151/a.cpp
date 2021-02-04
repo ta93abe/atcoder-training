032db445b77b4624abd4733a34089c6976b51ff2
Takumi Abe <contact@ta93abe.com>
2021-02-04 16:09:56 +0900

    solve abc151 a -- c++

---
+++ c++/abc/151/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    char c;
+    cin >> c;
+
+    cout << char(c + 1) << endl;
+
+    return 0;
+}
