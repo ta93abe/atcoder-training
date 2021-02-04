b156d600c04cf83c1ce0dd465358944fcd2c2139
Takumi Abe <contact@ta93abe.com>
2021-02-04 16:54:49 +0900

    solve abc158 a -- c++

---
+++ c++/abc/158/a.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    if (s == "AAA" or s == "BBB")
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+
+    return 0;
+}
