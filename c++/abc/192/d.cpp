53da08921a5e751033c434db97b39a8c7c073cef
Takumi Abe <contact@ta93abe.com>
2021-02-07 13:28:33 +0900

    solve abc191 a b -- c++

---
+++ c++/abc/192/d.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    int x, y, r;
+    cin >> x >> y >> r;
+
+    return 0;
+}
