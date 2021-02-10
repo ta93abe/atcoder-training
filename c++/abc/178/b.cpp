2499ecd22ad550e4b52782822229e1665cd78c10
Takumi Abe <ta93abe@ta93abe.com>
2021-02-10 22:46:05 +0900

    solve abc178 b -- c++

---
+++ c++/abc/178/b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << max(a * c, max(a * d, max(b * c, b * d))) << endl;
+
+    return 0;
+}
