ce7bebd2b48fbaa384ad35b9837487993f6836c4
Takumi Abe <ta93abe@ta93abe.com>
2021-02-11 09:42:42 +0900

    solve abc181 b -- c++

---
+++ c++/abc/181/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+using P = pair<int, int>;
+
+int main()
+{
+    ll n;
+    cin >> n;
+
+    ll cnt = 0;
+
+    rep(i, n)
+    {
+        ll a, b;
+        cin >> a >> b;
+        cnt += (a + b) * (b - a + 1) / 2;
+    }
+    cout << cnt << endl;
+
+    return 0;
+}
